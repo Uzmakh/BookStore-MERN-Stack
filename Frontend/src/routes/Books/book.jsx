ba1b1d2007aacb5b0e4e3917b13677ae96290d69
Uzmakh <urbazoha@gmail.com>
2023-09-30 17:03:09 +0500

    Created routes

---
+++ Frontend/src/routes/Books/book.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Books = () => {
+  return (
+   
+        <div>
+      
+      <h1>Books</h1>
+      <p>Minature Vite + React demo website.</p>
+      <p>It uses NodeJs, Express & MongoDB as a backend!</p>
+
+   
+    </div>
+  )
+}
+
+export default Books
