97d8471e308bde88aad12d57b879bd71aa3b716c
Uzmakh <urbazoha@gmail.com>
2023-10-07 09:46:01 +0500

    set the indentation of home.jsx

--- Frontend/src/routes/Home/home.jsx
+++ Frontend/src/routes/Home/home.jsx
@@ -2,14 +2,10 @@ import React from 'react'
 
 const Home = () => {
   return (
-   
-        <div>
-      
+    <div>
       <h1>Home</h1>
       <p>Minature Vite + React demo website.</p>
       <p>It uses NodeJs, Express & MongoDB as a backend!</p>
-
-   
     </div>
   )
 }
