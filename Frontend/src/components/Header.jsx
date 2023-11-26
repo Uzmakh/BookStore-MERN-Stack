6342cb74a239f63d35649032cca527690fe6b990
Uzmakh <urbazoha@gmail.com>
2023-11-26 20:14:00 +0500

    Changed the title and favicon plus Logo

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Link, NavLink } from "react-router-dom";
-import logo from "../assets/react.svg";
+import logo from "../assets/book-favicon.png";
 
 const Header = () => {
   return (
     <header>
           <Link to="/" className="logo">
-              <img src={logo} alt="ReactJs" /> ReactJs
+              <img src={logo} alt="ReactJs" /> BookApp
           </Link>
             <nav>
             <NavLink to="/">Home</NavLink>
